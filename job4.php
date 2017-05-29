8adbcc1feb969afa5b377afd3d7c16f74ee0110d
ahmadmuhajirin <ahmadmuajirin86@gmail.com>
2017-05-29 23:11:26 +0700

    Add files via upload

---
+++ job4.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>mengenal fungsi striing</title>
+</head>
+<body>
+<?php echo "<br>";
+		$pizza ="piece1 piece2 piece3 piece4 piece5 piece6";
+		$pieces = explode ("",$pizza);
+		echo "pieces [0] <br>";
+		echo "pieces [1] <br>";
+$data =  "foo:*:1023:1000::home/foo:/bin/sh";
+list ($use, $pass, $uid, $gid, $gecos, $home, $shell)= explode (":", $data);
+echo $user;
+echo $pass
+echo "</b>";?>
+</body>
+</html>
